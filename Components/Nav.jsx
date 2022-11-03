e2e1c626b2e51bd24a0e3d0d21b4b863ac7a1aa4
rishav8989 <betusenger@gmail.com>
2022-11-04 04:54:30 +0530

    navbar

--- Components/Nav.jsx
+++ Components/Nav.jsx
@@ -1,21 +1,30 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 
-import nav from '../styles/Nav.module.css'
+import nav from "../styles/Nav.module.css";
 
 function Nav() {
   return (
     <nav className={nav.nav}>
       <ul>
         <li>
-          <Link href='/' className={nav.btn}>Home</Link>
+          <Link href="/" className={nav.btn}>
+            Home
+          </Link>
         </li>
         <li>
-          <Link href='/' className={nav.btn}>About</Link>
+          <Link href="/about" className={nav.btn}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link href="/explore" className={nav.btn}>
+            Explore
+          </Link>
         </li>
       </ul>
     </nav>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
