65c6f90885c90bd95a1fcbe1c790cddfee503bc9
rishav8989 <betusenger@gmail.com>
2022-11-04 09:11:19 +0530

    FIRST sTEP

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -7,8 +8,15 @@ export default function Home() {
       <Head>
         <title>Rishav</title>
       </Head>
-      <h1>Rishav</h1>
-      <div className={styles.about_me}></div>
+      <div className={styles.about_me}>
+        <div className={styles.text}>
+        <p><span>Hello,</span> Myself Rishav</p>
+        <p>I am a Learner, I Studies and try different things, Subjects and Domains</p>
+        <p>You Can find about me more in ABOUT Section and things i do in EXPLORE section</p>
+        <p>No Buddy is Hiring me because i dont have experience in web deveplment. I dont have any experience because Nobody is Hiring me</p>
+        </div>
+      <Image src="/profile.jpeg" width="100" height="100"></Image>
+      </div>
     </div>
   )
 }
